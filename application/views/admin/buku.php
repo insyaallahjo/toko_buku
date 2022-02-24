7aad041c99269919559fda66d137e07257b245f1
Joko Indrawan <joko.indrawan@mindasaas.co.id>
2022-02-24 10:57:10 +0700

    first commit

---
+++ application/views/admin/buku.php
@@ -0,0 +1,46 @@
+
+        <div class="page-breadcrumb">
+                <div class="row align-items-center">
+                    <div class="col-5">
+                        <h4 class="page-title">Buku</h4>
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="<?php echo base_url('admin/p/home') ?>">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Buku</a></li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-7">
+                        <div class="text-right upgrade-btn">
+                            <a href="javascript:void(0)" class="btn btn-danger text-white" data-target="#tambah-buku" data-toggle="modal">Tambah buku</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table table-sm table-bordered" width="100%" id="table-buku">
+                                    <thead>
+                                        <tr>
+                                            <th>Judul</th>
+                                            <th>Kode Buku</th>
+                                            <th>Penulis</th>
+                                            <th>Tahun Terbit</th>
+                                            <th>Nomor ISBN</th>
+                                            <th>Stok</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
